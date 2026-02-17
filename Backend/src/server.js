327f8f143e48f588282a4875288c23fd640c658e
TheMLG <mlgoswami1801@gmail.com>
2026-02-17 20:02:25 +0530

    fix: update CORS middleware configuration and allow all origins chore: add .env to frontend .gitignore

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -14,10 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:5173', // your frontend URL
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
